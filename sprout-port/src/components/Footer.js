2c9044e9b8b2d80f0fa3851309a6cec8c52123d2
bennettem <emily.pamelabennett@gmail.com>
2022-07-16 09:31:51 -0500

    Footer component

---
+++ sprout-port/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer () {
+    return (
+        <div>
+            <h2> by yours truly</h2>
+            <div>
+                &copy; 2022
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
