c147e300e833ad6d8f554f18f53811e59b668fc0
bennettem <emily.pamelabennett@gmail.com>
2022-07-16 09:09:48 -0500

    Header/Nav component updated

---
+++ sprout-port/src/components/Nav.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+function Nav () {
+    return (
+        <div>
+            <span>
+                About me
+            </span>
+        </div>
+    );
+}
+
+export default Nav;
+
+
+
